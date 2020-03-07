ddeace0bd475b99848a044317d2ac1a2b2b24fe2
Evaldas <e.jurgaitis@ba.lt>
2020-03-07 15:39:10 +0200

    Initial customer service

---
+++ bms-customer-service/src/main/java/ej/BmsCustomerServiceApplication.java
@@ -0,0 +1,13 @@
+package ej;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BmsCustomerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BmsCustomerServiceApplication.class, args);
+	}
+
+}

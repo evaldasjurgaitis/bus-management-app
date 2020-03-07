05e7af83cee7cc6b8cbb3f96dab3b2468efa9ffe
Evaldas <e.jurgaitis@ba.lt>
2020-03-07 21:44:38 +0200

    Initial ticket service

---
+++ bms-ticket-service/README.md
@@ -0,0 +1,2 @@
+# BMS-application
+Ticket service
---
+++ bms-ticket-service/src/main/java/ej/BmsTicketServiceApplication.java
@@ -0,0 +1,13 @@
+package ej;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BmsTicketServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BmsTicketServiceApplication.class, args);
+    }
+
+}

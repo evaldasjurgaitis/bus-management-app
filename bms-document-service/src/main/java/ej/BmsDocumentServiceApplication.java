358dcc6a7f1a1b1d51cfd374d236d764bc0a653d
Evaldas <e.jurgaitis@ba.lt>
2020-03-07 21:48:48 +0200

    Initial document service

---
+++ bms-document-service/src/main/java/ej/BmsDocumentServiceApplication.java
@@ -0,0 +1,13 @@
+package ej;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BmsDocumentServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BmsDocumentServiceApplication.class, args);
+    }
+
+}
